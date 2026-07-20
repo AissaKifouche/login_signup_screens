d8753df7643468f451be1d0bebfc1d6afcdaa086
AissaKifouche <a_kifouche@estin.dz>
2026-07-20 19:32:33 +0100

    initialize project

---
+++ lib/login_screens/login_screen1.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen1 extends StatelessWidget {
+  const LoginScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "screen 1",
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:login_signup/login_screens/login_screen1.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginScreen1(),
+    );
+  }
+}
+
